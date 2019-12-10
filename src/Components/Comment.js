cb7ec54726c380e7d2a2c4a232cd7531130c7b0c
Evgenii Evstropov <john0606@yandex.ru>
2019-12-10 09:50:25 +0100

    solution for the task which was during webinar

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -3,6 +3,24 @@ import { Input, Text } from 'react-native-elements';
 import { TouchableOpacity } from 'react-native';
 
 export const Comment = () => {
-  // Ваш код здесь
-  return (<></>)
+  const [comment, setComment] = useState()
+  const [currentText, setCurrentText] = useState();
+  return (
+    <>
+      {comment && (
+        <Text h4 style={{ alignSelf: 'center'}}>{comment}</Text>
+      )}
+      <Input
+        value={currentText}
+        onChangeText={setCurrentText}
+        placeholder="Комментарий"
+        containerStyle={{ marginBottom: 20}}
+        rightIcon={{
+          Component:TouchableOpacity,
+          onPress: () => {setComment(currentText); setCurrentText('')},
+          name: 'sc-telegram',
+          type: 'evilicon',
+          color: '#517fa4'}}/>
+    </>
+  )
 }
